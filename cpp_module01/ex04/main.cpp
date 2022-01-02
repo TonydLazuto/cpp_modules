6c438cb025cf4e1ad1ed91b35ec61fe1a8486a21
TonydLazuto <aderose73@gmail.com>
2022-01-02 20:06:24 +0100

    sed for loosers

---
+++ cpp_module01/ex04/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tonyd <aderose73@gmail.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 16:20:13 by tonyd             #+#    #+#             */
+/*   Updated: 2022/01/02 16:20:14 by tonyd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+
+int	main(int ac, char *av[]) {
+	if (ac != 4)
+	{
+		std::cout << "Wrong numbers of arguments." << std::endl;
+		return 1;
+	}
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::string cur;
+	std::ifstream ifs(filename, std::ios_base::in);
+	
+	std::string newfile;
+	std::size_t found;
+	int i = 0;
+	while (getline(ifs, cur))
+	{
+		if (i > 0)
+			newfile.push_back('\n');
+		found = cur.find(s1);
+		while (found != std::string::npos)
+		{
+			cur.erase(found, s1.length());
+			cur.insert(found, s2);
+			found = cur.find(s1);
+		}
+			newfile += cur;
+		i++;
+	}
+	ifs.close();
+	filename += "2";
+	std::ofstream ofs(filename, std::ios_base::out | std::ios_base::trunc);
+	ofs << newfile ;
+	ofs.close();
+	return 0;
+}

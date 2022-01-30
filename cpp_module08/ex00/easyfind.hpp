95852d4e96a351b9366aab6c5ffee7362fefc308
Anthony DEROSE <aderose@42.student.fr>
2022-01-30 16:10:21 +0100

    07ex01

--- cpp_module08/ex00/easyfind.hpp
+++ cpp_module08/ex00/easyfind.hpp
@@ -3,24 +3,74 @@
 
 #include <iterator>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <typeinfo> // operator typeid
 
-template < typename T >
-void	easyfind(T const& container, int nb)
+class ContainerException: public std::exception 
 {
-	typename T::const_iterator it;
-	if (!*it)
-		throw std::exception();
-	for (it = container.begin();
-		it != container.end(); ++it)
+	virtual const char* what() const throw()
 	{
-		if (*it == nb)
+		return ("Not found.");
+	}
+};
+
+template<typename T>
+void	easyfind2( T &container, int nb)
+{
+	typename T::iterator val;
+
+	val = find(container.begin(), container.end(), nb);
+	if (val == container.end())
+		throw std::exception();
+	std::cout << "Value find in container: " << *val << std::endl;
+}
+
+template <>
+void	easyfind2(std::map<std::string, int>& map, int nb)
+{
+	std::map<std::string, int>::iterator it = map.begin();
+	while (it != map.end())
+	{
+		if (it->second == nb)
 		{
-			std::cout << "Find it!" << std::endl;
+			std::cout << "Value find in container_map: " << it->second << std::endl;
 			return ;
 		}
+		++it;
 	}
-	if (it == container.end())
+	if (it == map.end())
 		throw std::exception();
+}
+
+// template <>
+// void	easyfind2(std::stack<int> const& mystack, int nb)
+// {
+// 	(void)nb;
+// 	std::stack<int> *ptrstack = &(const_cast<std::stack<int>&>(mystack));
+// 	for (int i = 0; i < mystack.size(); i++)
+// 	{
+// 		// ptrstack->front();
+// 		std::cout << "Through stack : " << ptrstack->front() << std::endl;
+// 		// ptrstack -= ;
+		
+// 	}
+
+// 	if (!mystack.size())
+// 		throw std::exception();
+// }
+
+template < typename T >
+void	easyfind(T & container, int nb)
+{
+	if (typeid(T) == typeid(std::map<std::string, int>))
+		easyfind2<>(container, nb);
+	else
+		easyfind2<T>(container, nb);
 }
 
 #endif
--- cpp_module08/ex00/main.cpp
+++ cpp_module08/ex00/main.cpp
@@ -1,7 +1,5 @@
 #include "easyfind.hpp"
-#include <vector>
-#include <list>
-#include <stack>
+
 
 /**
  * Map and Set have multiple types
@@ -12,17 +10,32 @@ int	main(void)
 {
 	std::vector<int> v (4, 100);
 	std::list<int> lst(2);
+	std::deque<int> deq(2, 100);
 	lst.push_back(100);
 	lst.push_back(200);
 	lst.push_back(300);
 	lst.push_back(400);
 	
+	std::map<std::string, int> mymap;
+	mymap["one"] = 1;
+	mymap["two"] = 2;
+	mymap["three"] = 3;
+
+	int myints[]= {10,20,30,40,50};
 	std::stack<int> stack;
+	stack.push(myints[0]);
+	stack.push(myints[1]);
+	stack.push(myints[2]);
+	
+	std::set<int> myset (myints,myints+5);
 	try
 	{
 		easyfind< std::vector<int> >(v, 100);
-		easyfind< std::list<int> >(lst, 100);
-		easyfind< std::stack<int> >(stack, 100);
+		easyfind< std::list<int> >(lst, 200);
+		easyfind< std::deque<int> >(deq, 100);
+		easyfind< std::map<std::string, int> >(mymap, 2);
+		// easyfind< std::stack<int> >(stack, 30);
+		easyfind< std::set<int> >(myset, 30);
 	}
 	catch(const std::exception& e)
 	{
